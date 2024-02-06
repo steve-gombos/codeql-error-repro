115ba7b5d9b6e0e6ddb2a01d8e83d61b407002b1
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2024-02-06 17:48:21 -0500

    Test

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+namespace ClassLibrary2;
+
+public class Class1
+{
+    public void Test()
+    {
+        var settings = new XmlReaderSettings();
+        var reader = XmlReader.Create("test", settings);
+    }
+}
