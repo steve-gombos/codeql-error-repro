429a9b104315aeaa4a021ef1bafddd78d91fc960
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2024-02-07 00:35:17 -0500

    Match more of original

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -1,23 +1,38 @@
 ﻿using System.Text;
 using System.Xml;
-using System.Xml.Schema;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace ClassLibrary2;
 
-public class Class1 : XmlSerializerInputFormatter
+public class Class1<T> : XmlSerializerInputFormatter
 {
-    public Class1(MvcOptions options) : base(options)
+    private readonly MvcOptions _options;
+    private readonly int _maxErrors;
+
+    public Class1(MvcOptions options, int maxErrors = 5) : base(options)
     {
+        _options = options;
+        _maxErrors = maxErrors;
     }
 
+    protected override bool CanReadType(Type type)
+    {
+        var schemas = SchemaAccess.GetSchemaSet(type);
+        return schemas.Count > 0;
+    }
+    
     protected XmlReader CreateXmlReader(Stream readStream, Encoding encoding, InputFormatterContext context)
     {
         var errors = new List<string>();
+        var schemas = SchemaAccess.GetSchemaSet<T>();
         var settings = new XmlReaderSettings();
+        settings.DtdProcessing = DtdProcessing.Prohibit;
         settings.ValidationType = ValidationType.Schema;
-        settings.Schemas.Add(new XmlSchemaSet());
+        settings.Schemas.Add(schemas);
         settings.ValidationEventHandler += (sender, args) =>
         {
             errors.Add(args.Message);
@@ -25,25 +40,42 @@ public class Class1 : XmlSerializerInputFormatter
         };
         return XmlReader.Create(readStream, settings);
     }
-
-    public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+    
+    public async override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
     {
         if (context == null) throw new ArgumentNullException(nameof(context));
         if (encoding == null) throw new ArgumentNullException(nameof(encoding));
         
+        var request = context.HttpContext.Request;
+        var suppressInputFormatterBuffering = _options?.SuppressInputFormatterBuffering ?? false;
+
+        if (!request.Body.CanSeek && !suppressInputFormatterBuffering)
+        {
+            request.EnableBuffering();
+            await request.Body.DrainAsync(CancellationToken.None);
+            request.Body.Seek(0L, SeekOrigin.Begin);
+        }
+        
         try
         {
-            var request = context.HttpContext.Request;
-            using var xmlReader = CreateXmlReader(request.Body, encoding, context);
-            var type = GetSerializableType(context.ModelType);
-            var serializer = GetCachedSerializer(type);
-            var deserializedObject = serializer.Deserialize(xmlReader);
+            using(var xmlReader = CreateXmlReader(request.Body, encoding, context))
+            {
+                var type = GetSerializableType(context.ModelType);
+                var serializer = GetCachedSerializer(type);
+                var deserializedObject = serializer.Deserialize(xmlReader);
 
-            return InputFormatterResult.SuccessAsync(deserializedObject);
+                if (type != context.ModelType)
+                {
+                    if (deserializedObject is IUnwrappable unwrappable)
+                        deserializedObject = unwrappable.Unwrap(context.ModelType);
+                }
+
+                return await InputFormatterResult.SuccessAsync(deserializedObject);
+            }
         }
         catch (Exception)
         {
-            return InputFormatterResult.FailureAsync();
+            return await InputFormatterResult.FailureAsync();
         }
     }
 }
---
+++ ClassLibrary2/SchemaAccess.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Schema;
+
+namespace ClassLibrary2;
+
+public static class SchemaAccess
+{
+    public static XmlSchemaSet GetSchemaSet<T>()
+    {
+        var schemas = new XmlSchemaSet();
+        return schemas;
+    }
+    
+    public static XmlSchemaSet GetSchemaSet(Type type)
+    {
+        var schemas = new XmlSchemaSet();
+        return schemas;
+    }
+}
