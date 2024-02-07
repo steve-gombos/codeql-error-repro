de855e3f0b85d4effe226674c0d6d3d299a38ac6
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2024-02-07 00:00:29 -0500

    Add serializer

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -1,12 +1,35 @@
-﻿using System.Xml;
+﻿using System.Text;
+using System.Xml;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace ClassLibrary2;
 
-public class Class1
+public class Class1 : XmlSerializerInputFormatter
 {
-    public void Test(Stream stream)
+    public Class1(MvcOptions options) : base(options)
     {
-        var settings = new XmlReaderSettings();
-        var reader = XmlReader.Create(stream, settings);
+    }
+
+    public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+    {
+        if (context == null) throw new ArgumentNullException(nameof(context));
+        if (encoding == null) throw new ArgumentNullException(nameof(encoding));
+        
+        try
+        {
+            var request = context.HttpContext.Request;
+            var settings = new XmlReaderSettings();
+            using var xmlReader = XmlReader.Create(request.Body, settings);
+            var type = GetSerializableType(context.ModelType);
+            var serializer = GetCachedSerializer(type);
+            var deserializedObject = serializer.Deserialize(xmlReader);
+
+            return InputFormatterResult.SuccessAsync(deserializedObject);
+        }
+        catch (Exception)
+        {
+            return InputFormatterResult.FailureAsync();
+        }
     }
 }
